7695101215c7a7691fa93f4f259e42de06b2d875
mihawk <chan.sisowath@gmail.com>
2017-02-20 17:46:32 +0700

    add field_types,rename fields to field_names

--- src/dynarec.erl
+++ src/dynarec.erl
@@ -47,12 +47,12 @@
 parse_transform(Forms, _Options) ->
     Records = [Record || {attribute, _, record, _} = Record <- Forms],
     Tuples = lists:flatten(lists:map(fun get_tuples/1, Records)),
-    NewForms = lists:reverse([gen_new_record(Tuples) |
-                              [gen_records(Tuples) |
-                               [gen_fields(Tuples) |
-                                [gen_field_getter(Tuples) |
-                                 [gen_setter(Tuples) |
-                                  lists:reverse(Forms)]]]]]),
+    NewForms = lists:reverse([gen_new_record(Tuples)  |
+                             [gen_records(Tuples)     |
+                             [gen_field_names(Tuples) |
+                             [gen_field_types(Tuples) |
+                             [gen_field_getter(Tuples)|
+                             [gen_setter(Tuples)      | lists:reverse(Forms)]]]]]]),
     add_exports(NewForms).
 
 
@@ -66,16 +66,34 @@ get_field_name({record_field, _, {atom, _, FieldName}, _Default}) ->
 get_field_name({record_field, _, {atom, _, FieldName}}) ->
     FieldName;
 get_field_name({typed_record_field, RecordField, _Typed}) ->
-    get_field_name(RecordField).
+    %io:format("~p ~p~n",[RecordField, _Typed]),
+    {get_field_name(RecordField), get_type(_Typed)}.
 
+get_type({type,_,tuple,L})     -> {tuple,tuple(L)};
+get_type({type,_,Type,[]})     -> Type;
+get_type({user_type,_,Type,[]})-> Type;
+get_type({type,_,union,Union}) -> {union, union(Union)}.
+
+union(L) ->
+    lists:foldr(fun({K,_,V},A)      -> [{K,V}|A];
+                   ({type,_,T,[]},A) -> [T|A];
+                   ({user_type,_,T,[]},A) -> [T|A] 
+                end,[],L).
+
+list(L) -> lists:foldr(fun({type,_,range,R},A) -> [range(R)|A]end,[],L).
+tuple(L) -> lists:foldr(fun({type,_,range,R},A) -> [range(R)|A]end,[],L).
+
+range([{integer,_,Start},{integer,_,End}]) -> {range,integer,{Start,End}}.
 
 %% @doc Generates the field_getter function for all {record, field} tuples.
 %% <p>It returns code in the syntactic tree form. You can use
 %% <code>erl_scan:tokens/3</code> and <code>erl_parse:parse_form/1</code> to
 %% see how it parses different strings.</p>
 gen_field_getter(Tuples) ->
-    List = lists:foldl(fun({Record, Field}, Acc) ->
-                               [gen_field_getter_clause(Record, Field) | Acc]
+    List = lists:foldl(fun({Record, {Field,_}}, Acc) ->
+                               [gen_field_getter_clause(Record, Field) | Acc];
+                          ({Record, Field}, Acc) ->
+                               [gen_field_getter_clause(Record, Field) | Acc]                            
                        end, [], Tuples),
     {function, 0, get_value, 2, lists:reverse(List)}.
 
@@ -94,6 +112,15 @@ gen_field_getter(Tuples) ->
 %%      erl_scan:tokens([],"get_value(field_name, Record) -> Record#record_name.field_name.\n",0)))).
 %% </pre>
 %% <p>It first tokenizes the string as code, and then builds the syntactic tree.</p>
+
+% {ok,{function,0,get_value,2,
+%               [{clause,0,
+%                        [{atom,0,field_name},{var,0,'Record'}],
+%                        [],
+%                        [{record_field,0,
+%                                       {var,0,'Record'},
+%                                       record_name,
+%                                       {atom,0,field_name}}]}]}}
 gen_field_getter_clause(RecordName, FieldName) ->
     {clause, 0,
      %% (field_name, Record)
@@ -108,7 +135,9 @@ gen_field_getter_clause(RecordName, FieldName) ->
 %% <code>erl_scan:tokens/3</code> and <code>erl_parse:parse_form/1</code> to
 %% see how it parses different strings.</p>
 gen_setter(Tuples) ->
-    List = lists:foldl(fun({Record, Field}, Acc) ->
+    List = lists:foldl(fun({Record, {Field,_}}, Acc) ->
+                               [gen_setter_clause(Record, Field) | Acc];
+                          ({Record, Field}, Acc) ->
                                [gen_setter_clause(Record, Field) | Acc]
                        end, [], Tuples),
     {function, 0, set_value, 3, lists:reverse(List)}.
@@ -166,20 +195,44 @@ gen_records(Tuples) ->
 %% <pre>
 %% erl_scan:tokens([], "fields(Record) -> [value, othervalue, etc].\n", 0).
 %% </pre>
-gen_fields(Tuples) ->
+gen_field_types(Tuples) ->
     %% Group fields by record
     %% e.g. [{site, id}, {site, country}, {item, id}, {item, price}] --> [{site, [id, country]}, {item, [id, price]}]
-    Grouped = dict:to_list(lists:foldl(fun({Name, Field}, Dict) -> dict:append(Name, Field, Dict) end, dict:new(), Tuples)),
-    List = [{dot, 0} | tl(lists:foldl(fun({Record, Fields}, Acc) -> gen_reversed_fields_tokens(Record, Fields) ++ Acc end, [], Grouped))],
+    Grouped = dict:to_list(lists:foldl(fun({Name, X}, Dict) -> 
+                                            dict:append(Name, X, Dict)                                          
+                                       end, dict:new(), Tuples)),
+
+    List = [{dot, 0} | tl(lists:foldl(fun({Record, Fields}, Acc) -> 
+                                        gen_reversed_field_types_tokens(Record, Fields) ++ Acc 
+                                      end, [], Grouped))],
     {ok, Result} = erl_parse:parse_form(lists:reverse(List)),
     Result.
 
-gen_reversed_fields_tokens(Record, Fields) ->
+gen_reversed_field_types_tokens(Record, Fields) ->
     %% Reversed tokens for the expression: fields(Record) ->
-    ReversedPrefix = [{'->', 0}, {')', 0}, {atom, 0, Record}, {'(', 0}, {atom, 0, fields}],
+    ReversedPrefix = [{'->', 0}, {')', 0}, {atom, 0, Record}, {'(', 0}, {atom, 0, field_types}],
     [{';', 0} | gen_reversed_atom_list(Fields, ReversedPrefix)].
 
 
+gen_field_names(Tuples) ->
+    %% Group fields by record
+    %% e.g. [{site, id}, {site, country}, {item, id}, {item, price}] --> [{site, [id, country]}, {item, [id, price]}]
+    Grouped = dict:to_list(lists:foldl(fun({Name, {Field,_}}, Dict) -> 
+                                            dict:append(Name, Field, Dict);
+                                          ({Name, Field}, Dict) -> 
+                                            dict:append(Name, Field, Dict)  
+                                       end, dict:new(), Tuples)),
+
+    List = [{dot, 0} | tl(lists:foldl(fun({Record, Fields}, Acc) -> 
+                                        gen_reversed_field_names_tokens(Record, Fields) ++ Acc 
+                                      end, [], Grouped))],
+    {ok, Result} = erl_parse:parse_form(lists:reverse(List)),
+    Result.
+
+gen_reversed_field_names_tokens(Record, Fields) ->
+    %% Reversed tokens for the expression: fields(Record) ->
+    ReversedPrefix = [{'->', 0}, {')', 0}, {atom, 0, Record}, {'(', 0}, {atom, 0, field_names}],
+    [{';', 0} | gen_reversed_atom_list(Fields, ReversedPrefix)].
 
 %% @doc Generates the new_record function
 %%
@@ -199,13 +252,12 @@ gen_new_record(Tuples) ->
 gen_new_record_clause(RecordName) ->
     {clause, 0, [{atom, 0, RecordName}], [], [{record, 0, RecordName, []}]}.
 
-
 %% @doc Adds the getter and setter functions to the list of exported functions
 add_exports(Forms) ->
     case lists:keysearch(export, 3, Forms) of
         {value, {attribute, N, export, Exports}} ->
             lists:keyreplace(export, 3, Forms,
-                {attribute, N, export, [{records, 0}, {fields, 1},
+                {attribute, N, export, [{records, 0}, {field_names, 1}, {field_types, 1},
                 {new_record, 1}, {get_value, 2}, {set_value, 3} | Exports]});
         false -> Forms
     end.
